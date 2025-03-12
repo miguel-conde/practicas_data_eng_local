f055c94e0736330f812c5316085f2bda709b577d
Miguel Conde <miguelco2000@gmail.com>
2025-03-12 07:12:20 +0100

    Refactoring data_cleaning.py

--- cleaning/data_cleaning.py
+++ cleaning/data_cleaning.py
@@ -1,18 +1,4 @@
-# import os
-# import pandas as pd
-# from minio import Minio
-# from io import BytesIO
-# from dotenv import load_dotenv
-
 from dl_manager import DLManager
-
-# load_dotenv("../config/.env")
-
-# # Credenciales MinIO
-# MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
-# MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
-# MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY')
-# MINIO_BUCKET = os.getenv('MINIO_BUCKET')
 
 def clean_kaggle_diabetes_dataset(df):
     # --------- Limpieza del dataset (tu lógica aquí) ---------
@@ -39,63 +25,37 @@ def clean_kaggle_diabetes_dataset(df):
     
     return df_clean
 
-# # Conexión a MinIO
-# client = Minio(
-#     MINIO_ENDPOINT,
-#     access_key=MINIO_ACCESS_KEY,
-#     secret_key=MINIO_SECRET_KEY,
-#     secure=False  # cambia a True si usas https
-# )
-
-# # Lee el CSV desde MinIO (raw)
-# raw_object_name = "raw/diabetes_dataset/diabetes_dataset.csv"  # Cambia por tu nombre real
-# response = client.get_object(MINIO_BUCKET, raw_object_name)
-# df = pd.read_csv(response)
-
-# # --------- Limpieza del dataset (tu lógica aquí) ---------
-
-# # `Outcome` is the target variable, must be boolean
-# df_clean = clean_kaggle_diabetes_dataset(df)
-
-
-# # ---------------------------------------------------------
-
-# # Guarda el resultado limpio en MinIO (clean)
-# clean_csv = df_clean.to_csv(index=False).encode('utf-8')
-# clean_object_name = "clean/diabetes_dataset/diabetes_dataset_clean.csv"
-
-# client.put_object(
-#     bucket_name=MINIO_BUCKET,
-#     object_name=clean_object_name,
-#     data=BytesIO(clean_csv),
-#     length=len(clean_csv),
-#     content_type='application/csv'
-# )
-
-# print(f"Dataset limpio guardado correctamente en MinIO: {clean_object_name}")
-
-def clean_process(raw_object_name, clean_object_name):
+def clean_process(cleaning_list):
     
     dl_manager = DLManager()
     
-    # Lee el CSV desde MinIO (raw)
-    raw_object_name = raw_object_name
-    df = dl_manager.get_csv(raw_object_name)
+    for cleaning_info in cleaning_list:
+        raw_object_name   = cleaning_info["raw_object_name"]
+        clean_object_name = cleaning_info["clean_object_name"]
+        cleaning_function = cleaning_info["cleaning_function"]
     
-    # Data cleaning
-    df_clean = clean_kaggle_diabetes_dataset(df)
+        # Lee el CSV desde MinIO (raw)
+        raw_object_name = raw_object_name
+        df = dl_manager.get_csv(raw_object_name)
+        
+        # Data cleaning
+        df_clean = cleaning_function(df)
+        
+        # Guarda el resultado limpio en MinIO (clean)
+        dl_manager.put_csv(df_clean, clean_object_name)
+        
+        print(f"Dataset limpio guardado correctamente en MinIO: {clean_object_name}")
     
-    # Guarda el resultado limpio en MinIO (clean)
-    dl_manager.put_csv(df_clean, clean_object_name)
+      
+if __name__ == "__main__":
     
-    print(f"Dataset limpio guardado correctamente en MinIO: {clean_object_name}")
+    cleaning_list = [
+        {
+            "raw_object_name": "raw/diabetes_dataset/diabetes_dataset.csv",
+            "clean_object_name": "clean/diabetes_dataset/diabetes_dataset_clean.csv",
+            "cleaning_function": clean_kaggle_diabetes_dataset
+        }
+    ]
     
-    
-if __name__ == "main":
-    
-    clean_process(
-        raw_object_name   = "raw/diabetes_dataset/diabetes_dataset.csv",
-        clean_object_name = "clean/diabetes_dataset/diabetes_dataset_clean.csv"
-    )
-    
+    clean_process(cleaning_list = cleaning_list)
     
